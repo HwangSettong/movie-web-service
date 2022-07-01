524400d133f403d95c03fc79e7570f74766f723c
SettongHwang <devsehyun@gmail.com>
2022-07-01 18:00:51 +0900

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useEffect} from "react";
+import "./style/style.css"
+require('dotenv').config()
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([])
+  const getMovies = async() => {
+    const json = await (await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )).json()
+    console.log(json)
+    setMovies(json.data.movies);
+    
+    setLoading(false);
+  }
+  useEffect(() => {
+    getMovies();
+    }, [])
+  console.log(movies)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? <h1>Loading...</h1> :
+      <div>
+        <table>
+          <th></th>
+          <th>Title</th>
+          <th>Genre</th>
+          {movies.map(item =>
+            <tr key={item.id}>
+              <td><img className="poster" src={item.large_cover_image}/></td>
+              <td>{item.title_long}</td>
+              <td>{item.genres.join(", ")}</td>
+
+          </tr>)}
+        </table>
+      </div>
+      
+      }
     </div>
   );
 }
