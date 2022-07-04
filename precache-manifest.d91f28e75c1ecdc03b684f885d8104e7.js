f0181a16300eef3f6fa794f9e539bb76a0125d44
SettongHwang <devsehyun@gmail.com>
2022-07-04 16:03:26 +0900

    Updates

---
+++ precache-manifest.d91f28e75c1ecdc03b684f885d8104e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f66d10243a86752481a",
+    "url": "/movie-web-service/static/css/main.8ebd7ce2.chunk.css"
+  },
+  {
+    "revision": "3f66d10243a86752481a",
+    "url": "/movie-web-service/static/js/main.3f66d102.chunk.js"
+  },
+  {
+    "revision": "ced9c7348a47cc4ad8f3",
+    "url": "/movie-web-service/static/js/1.ced9c734.chunk.js"
+  },
+  {
+    "revision": "a0a30971b3c52476e551",
+    "url": "/movie-web-service/static/js/runtime~main.a0a30971.js"
+  },
+  {
+    "revision": "c6f598a159f5fe8e2055041e84c2e465",
+    "url": "/movie-web-service/index.html"
+  }
+];
